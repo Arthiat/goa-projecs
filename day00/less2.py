5cae28fda899fe3ad684a336d93253dd01503127
Arthiat <arthurraluri@gmail.com>
2025-05-25 17:19:13 +0400

    fourth commit

---
+++ day00/less2.py
@@ -0,0 +1,11 @@
+name = "mirian"
+text1 = "me var"
+surname = "gorelashvili"
+age = 16 
+text2 = "wlis"
+height = 175.6
+text3 = "simagle"
+Knows_Programming = True
+
+print(text1 + " " + name + " " + surname + " " + str(age) + " " + text2 + " " + text3 + " " + str(height))
+
